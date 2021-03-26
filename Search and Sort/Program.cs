614cb79550c72e6923ee0606211ce93e8707556b
Stewart Charles Fisher <25020928@students.lincoln.ac.uk>
2021-03-26 13:30:35 +0000

    Basic Ascending Order Sort Template

--- Search and Sort/Program.cs
+++ Search and Sort/Program.cs
@@ -4,22 +4,28 @@ namespace Search_and_Sort
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             // Ask the user for the file paths to each of the respective data sets and sort them into individual arrays
             Console.Write("Please input the file path for Road_1_256.txt: ");
             string roadOneFileData = Console.ReadLine();
-            string[] roadOne = System.IO.File.ReadAllLines(roadOneFileData);
-
+            string[] roadOneString = System.IO.File.ReadAllLines(roadOneFileData);
+            int[] roadOne = Array.ConvertAll(roadOneString, int.Parse);
+            Array.Sort(roadOne);
+           
             Console.Write("Please input the file path for Road_2_256.txt: ");
             string roadTwoFileData = Console.ReadLine();
-            string[] roadTwo = System.IO.File.ReadAllLines(roadTwoFileData);
+            string[] roadTwoString = System.IO.File.ReadAllLines(roadTwoFileData);
+            int[] roadTwo = Array.ConvertAll(roadTwoString, int.Parse);
+            Array.Sort(roadTwo);
 
             Console.Write("Please input the file path for Road_3_256.txt: ");
             string roadThreeFileData = Console.ReadLine();
-            string[] roadThree = System.IO.File.ReadAllLines(roadThreeFileData);
-
-
+            string[] roadThreeString = System.IO.File.ReadAllLines(roadThreeFileData);
+            int[] roadThree = Array.ConvertAll(roadThreeString, int.Parse);
+            Array.Sort(roadThree);
         }
+
+        
     }
 }
