462a8c83250f5f49cb72ab1abfa5b42d727d2030
Stewart Charles Fisher <25020928@students.lincoln.ac.uk>
2021-03-26 13:00:28 +0000

    Add project files.

---
+++ Search and Sort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Search_and_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for the file paths to each of the respective data sets and sort them into individual arrays
+            Console.Write("Please input the file path for Road_1_256.txt: ");
+            string roadOneFileData = Console.ReadLine();
+            string[] roadOne = System.IO.File.ReadAllLines(roadOneFileData);
+
+            Console.Write("Please input the file path for Road_2_256.txt: ");
+            string roadTwoFileData = Console.ReadLine();
+            string[] roadTwo = System.IO.File.ReadAllLines(roadTwoFileData);
+
+            Console.Write("Please input the file path for Road_3_256.txt: ");
+            string roadThreeFileData = Console.ReadLine();
+            string[] roadThree = System.IO.File.ReadAllLines(roadThreeFileData);
+
+
+        }
+    }
+}
