e89256dad83696b48d2d24962665fedaaabe45ea
Stewart Charles Fisher <25020928@students.lincoln.ac.uk>
2021-03-27 14:30:44 +0000

    Reconfigured Ascending and Descending Order Sorts

--- Search and Sort/Program.cs
+++ Search and Sort/Program.cs
@@ -11,7 +11,7 @@ namespace Search_and_Sort
             string roadOneFileData = Console.ReadLine();
             string[] roadOneString = System.IO.File.ReadAllLines(roadOneFileData);
             int[] roadOne = Array.ConvertAll(roadOneString, int.Parse);
-                       
+                                   
             Console.Write("Please input the file path for Road_2_256.txt: ");
             string roadTwoFileData = Console.ReadLine();
             string[] roadTwoString = System.IO.File.ReadAllLines(roadTwoFileData);
@@ -20,22 +20,44 @@ namespace Search_and_Sort
             Console.Write("Please input the file path for Road_3_256.txt: ");
             string roadThreeFileData = Console.ReadLine();
             string[] roadThreeString = System.IO.File.ReadAllLines(roadThreeFileData);
-            int[] roadThree = Array.ConvertAll(roadThreeString, int.Parse);
-            Array.Sort(roadThree);
+            int[] roadThree = Array.ConvertAll(roadThreeString, int.Parse);            
         }
 
         private static void AscendingSort(int[] arr)
         {
             // Sort the array in ascending order
-            Array.Sort(arr);
+            int z;
+            for (int p = 0; p <= arr.Length - 2; p++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        z = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = z;
+                    }
+                }
+            }
             Console.WriteLine(string.Join(", ", arr));
         }
 
         private static void DescendingSort(int[] arr)
         {
             // Sort the array in descending order
-            Array.Sort(arr);
-            Array.Reverse(arr);
+            int z;
+            for (int p = 0; p <= arr.Length - 2; p++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] < arr[i + 1])
+                    {
+                        z = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = z;
+                    }
+                }
+            }
             Console.WriteLine(string.Join(", ", arr));
         }
 
@@ -46,19 +68,42 @@ namespace Search_and_Sort
             
             if (tenthValueResponse == "Ascending")
             {
-                Array.Sort(arr);
+                int z;
+                for (int p = 0; p <= arr.Length - 2; p++)
+                {
+                    for (int i = 0; i <= arr.Length - 2; i++)
+                    {
+                        if (arr[i] > arr[i + 1])
+                        {
+                            z = arr[i + 1];
+                            arr[i + 1] = arr[i];
+                            arr[i] = z;
+                        }
+                    }
+                }
                 Console.WriteLine("The values you have selected are:");
-                for (int i = 10; i < 260; i = i + 10)
+                for (int i = 10; i < arr.Length; i = i + 10)
                 {
                     Console.WriteLine(arr[i]);
                 }                
             }
             else if (tenthValueResponse == "Descending")
             {
-                Array.Sort(arr);
-                Array.Reverse(arr);
+                int z;
+                for (int p = 0; p <= arr.Length - 2; p++)
+                {
+                    for (int i = 0; i <= arr.Length - 2; i++)
+                    {
+                        if (arr[i] < arr[i + 1])
+                        {
+                            z = arr[i + 1];
+                            arr[i + 1] = arr[i];
+                            arr[i] = z;
+                        }
+                    }
+                }
                 Console.WriteLine("The values you have selected are:");
-                for (int i = 10; i < 260; i = i + 10)
+                for (int i = 10; i < arr.Length; i = i + 10)
                 {
                     Console.WriteLine(arr[i]);
                 }
@@ -66,7 +111,7 @@ namespace Search_and_Sort
             else if (tenthValueResponse == "No sort")
             {
                 Console.WriteLine("The values you have selected are:");
-                for (int i = 10; i < 260; i = i + 10)
+                for (int i = 10; i < arr.Length; i = i + 10)
                 {
                     Console.WriteLine(arr[i]);
                 }
@@ -77,6 +122,11 @@ namespace Search_and_Sort
                 Console.WriteLine("Try again");
                 DisplayTenthValues(arr);
             }           
+        }
+
+        private static void UserSearch(int[] arr)
+        {
+
         }
         
     }
