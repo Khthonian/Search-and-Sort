6b19eae7941b21bdead2c6dece9216a3d160d219
Stewart Charles Fisher <25020928@students.lincoln.ac.uk>
2021-03-27 18:42:50 +0000

    Method for UserSearch

--- Search and Sort/Program.cs
+++ Search and Sort/Program.cs
@@ -11,6 +11,7 @@ namespace Search_and_Sort
             string roadOneFileData = Console.ReadLine();
             string[] roadOneString = System.IO.File.ReadAllLines(roadOneFileData);
             int[] roadOne = Array.ConvertAll(roadOneString, int.Parse);
+            Console.WriteLine(UserSearch(roadOne, 112));
                                    
             Console.Write("Please input the file path for Road_2_256.txt: ");
             string roadTwoFileData = Console.ReadLine();
@@ -63,9 +64,11 @@ namespace Search_and_Sort
 
         private static void DisplayTenthValues(int[] arr)
         {
+            // Ask user if they would like to sort the values before printing to console
             Console.WriteLine("How would you like to sort the data:\n1)Ascending\n2)Descending\n3)No sort\nResponse: ");
             string tenthValueResponse = Console.ReadLine();
             
+            // The Ascending system is reused here
             if (tenthValueResponse == "Ascending")
             {
                 int z;
@@ -81,12 +84,14 @@ namespace Search_and_Sort
                         }
                     }
                 }
-                Console.WriteLine("The values you have selected are:");
+                // Print each tenth iteration until the max value has been reached
+                Console.WriteLine("The values you have selected are:");                
                 for (int i = 10; i < arr.Length; i = i + 10)
                 {
                     Console.WriteLine(arr[i]);
                 }                
             }
+            // The Descending system has been reused here
             else if (tenthValueResponse == "Descending")
             {
                 int z;
@@ -102,6 +107,7 @@ namespace Search_and_Sort
                         }
                     }
                 }
+                // Print each tenth iteration until the max value has been reached
                 Console.WriteLine("The values you have selected are:");
                 for (int i = 10; i < arr.Length; i = i + 10)
                 {
@@ -110,6 +116,7 @@ namespace Search_and_Sort
             }
             else if (tenthValueResponse == "No sort")
             {
+                // Print each tenth iteration until the max value has been reached
                 Console.WriteLine("The values you have selected are:");
                 for (int i = 10; i < arr.Length; i = i + 10)
                 {
@@ -124,10 +131,61 @@ namespace Search_and_Sort
             }           
         }
 
-        private static void UserSearch(int[] arr)
+        private static int UserSearch(int[] arr, int searchTarget)
         {
+            {
+                int maxIndex = arr.Length;
+                // Check if the user's given number is outside the values of the array
+                if (searchTarget <= arr[0])
+                    return arr[0];
+                if (searchTarget >= arr[maxIndex - 1])
+                    return arr[maxIndex - 1];
 
+                // Begin doing a binary search to find the closest value 
+                int i = 0, j = maxIndex, midValue = 0;
+                while (i < j)
+                {
+                    midValue = (i + j) / 2;
+
+                    if (arr[midValue] == searchTarget)
+                        return arr[midValue];
+
+                    // If the given number is below the midpoint, it will search below this point
+                    if (searchTarget < arr[midValue])
+                    {
+
+                        // Find the closest value if the given number is greater than the number an index before the midpoint
+                        if (midValue > 0 && searchTarget > arr[midValue - 1])
+                            return GetClosestValue(arr[midValue - 1],
+                                         arr[midValue], searchTarget);
+
+                        // Repeat this again in the lower sector
+                        j = midValue;
+                    }
+
+                    // If the given number is greater than the midpoint, it will search above this point
+                    else
+                    {
+                        // Find the closest value if the given number is lower than the number an index after the midpoint
+                        if (midValue < maxIndex - 1 && searchTarget < arr[midValue + 1])
+                            return GetClosestValue(arr[midValue],
+                                 arr[midValue + 1], searchTarget);
+                        i = midValue + 1;
+                    }
+                }
+
+                // Return the only single element remaining after the search     
+                return arr[midValue];
+            }            
         }
-        
+
+        public static int GetClosestValue(int valueOne, int valueTwo, int searchTarget)
+        {
+            // Determine between two values which is closest to the given number
+            if (searchTarget - valueOne >= valueTwo - searchTarget)
+                return valueTwo;
+            else
+                return valueOne;
+        }
     }
 }
