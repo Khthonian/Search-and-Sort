359b9efe7c56d2e8ffdaef469974c1f7a9fcda56
Stewart Charles Fisher <25020928@students.lincoln.ac.uk>
2021-03-28 14:42:30 +0100

    Reconfigured UserSearch and added Menus

--- Search and Sort/Program.cs
+++ Search and Sort/Program.cs
@@ -11,8 +11,7 @@ namespace Search_and_Sort
             string roadOneFileData = Console.ReadLine();
             string[] roadOneString = System.IO.File.ReadAllLines(roadOneFileData);
             int[] roadOne = Array.ConvertAll(roadOneString, int.Parse);
-            Console.WriteLine(UserSearch(roadOne, 112));
-                                   
+                                                           
             Console.Write("Please input the file path for Road_2_256.txt: ");
             string roadTwoFileData = Console.ReadLine();
             string[] roadTwoString = System.IO.File.ReadAllLines(roadTwoFileData);
@@ -21,7 +20,25 @@ namespace Search_and_Sort
             Console.Write("Please input the file path for Road_3_256.txt: ");
             string roadThreeFileData = Console.ReadLine();
             string[] roadThreeString = System.IO.File.ReadAllLines(roadThreeFileData);
-            int[] roadThree = Array.ConvertAll(roadThreeString, int.Parse);            
+            int[] roadThree = Array.ConvertAll(roadThreeString, int.Parse);
+
+            Console.Write("Please input the file path for Road_1_2048.txt: ");
+            string roadOne2048FileData = Console.ReadLine();
+            string[] roadOne2048String = System.IO.File.ReadAllLines(roadOne2048FileData);
+            int[] roadOne2048 = Array.ConvertAll(roadOne2048String, int.Parse);
+
+            Console.Write("Please input the file path for Road_2_2048.txt: ");
+            string roadTwo2048FileData = Console.ReadLine();
+            string[] roadTwo2048String = System.IO.File.ReadAllLines(roadTwo2048FileData);
+            int[] roadTwo2048 = Array.ConvertAll(roadTwo2048String, int.Parse);
+
+            Console.Write("Please input the file path for Road_3_2048.txt: ");
+            string roadThree2048FileData = Console.ReadLine();
+            string[] roadThree2048String = System.IO.File.ReadAllLines(roadThree2048FileData);
+            int[] roadThree2048 = Array.ConvertAll(roadThree2048String, int.Parse);
+
+            // Ask the user which data set they would like to use
+            DataMenu(roadOne, roadTwo, roadThree, roadOne2048, roadTwo2048, roadThree2048);           
         }
 
         private static void AscendingSort(int[] arr)
@@ -40,6 +57,8 @@ namespace Search_and_Sort
                     }
                 }
             }
+            Console.WriteLine();
+            Console.WriteLine("The values you have requested are:");
             Console.WriteLine(string.Join(", ", arr));
         }
 
@@ -59,86 +78,194 @@ namespace Search_and_Sort
                     }
                 }
             }
+            Console.WriteLine();
+            Console.WriteLine("The values you have requested are:");
             Console.WriteLine(string.Join(", ", arr));
         }
 
-        private static void DisplayTenthValues(int[] arr)
+        public static void DisplayIterativeValues(int[] arr)
         {
-            // Ask user if they would like to sort the values before printing to console
-            Console.WriteLine("How would you like to sort the data:\n1)Ascending\n2)Descending\n3)No sort\nResponse: ");
-            string tenthValueResponse = Console.ReadLine();
-            
-            // The Ascending system is reused here
-            if (tenthValueResponse == "Ascending")
+            int dataLength = arr.Length;
+
+            if (dataLength == 256)
             {
-                int z;
-                for (int p = 0; p <= arr.Length - 2; p++)
+                // Ask user if they would like to sort the values before printing to console
+                Console.WriteLine();
+                Console.WriteLine("How would you like to sort the data:\n1) Ascending\n2) Descending\n3) No sort\nResponse: ");
+                string tenthValueResponse = Console.ReadLine();
+
+                // The Ascending system is reused here
+                if (tenthValueResponse == "Ascending")
                 {
-                    for (int i = 0; i <= arr.Length - 2; i++)
+                    int z;
+                    for (int p = 0; p <= arr.Length - 2; p++)
                     {
-                        if (arr[i] > arr[i + 1])
+                        for (int i = 0; i <= arr.Length - 2; i++)
                         {
-                            z = arr[i + 1];
-                            arr[i + 1] = arr[i];
-                            arr[i] = z;
+                            if (arr[i] > arr[i + 1])
+                            {
+                                z = arr[i + 1];
+                                arr[i + 1] = arr[i];
+                                arr[i] = z;
+                            }
                         }
                     }
-                }
-                // Print each tenth iteration until the max value has been reached
-                Console.WriteLine("The values you have selected are:");                
-                for (int i = 10; i < arr.Length; i = i + 10)
-                {
-                    Console.WriteLine(arr[i]);
-                }                
-            }
-            // The Descending system has been reused here
-            else if (tenthValueResponse == "Descending")
-            {
-                int z;
-                for (int p = 0; p <= arr.Length - 2; p++)
-                {
-                    for (int i = 0; i <= arr.Length - 2; i++)
+                    // Print each tenth iteration until the max value has been reached
+                    Console.WriteLine();
+                    Console.WriteLine("The values you have selected are:");
+                    for (int i = 10; i < arr.Length; i = i + 10)
                     {
-                        if (arr[i] < arr[i + 1])
-                        {
-                            z = arr[i + 1];
-                            arr[i + 1] = arr[i];
-                            arr[i] = z;
-                        }
+                        Console.WriteLine(arr[i]);
                     }
                 }
-                // Print each tenth iteration until the max value has been reached
-                Console.WriteLine("The values you have selected are:");
-                for (int i = 10; i < arr.Length; i = i + 10)
+                // The Descending system has been reused here
+                else if (tenthValueResponse == "Descending")
                 {
-                    Console.WriteLine(arr[i]);
+                    int z;
+                    for (int p = 0; p <= arr.Length - 2; p++)
+                    {
+                        for (int i = 0; i <= arr.Length - 2; i++)
+                        {
+                            if (arr[i] < arr[i + 1])
+                            {
+                                z = arr[i + 1];
+                                arr[i + 1] = arr[i];
+                                arr[i] = z;
+                            }
+                        }
+                    }
+                    // Print each tenth iteration until the max value has been reached
+                    Console.WriteLine();
+                    Console.WriteLine("The values you have selected are:");
+                    for (int i = 10; i < arr.Length; i = i + 10)
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
+                }
+                else if (tenthValueResponse == "No sort")
+                {
+                    // Print each tenth iteration until the max value has been reached
+                    Console.WriteLine();
+                    Console.WriteLine("The values you have selected are:");
+                    for (int i = 10; i < arr.Length; i = i + 10)
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Try again");
+                    DisplayIterativeValues(arr);
                 }
             }
-            else if (tenthValueResponse == "No sort")
+
+            if (dataLength == 2048)
             {
-                // Print each tenth iteration until the max value has been reached
-                Console.WriteLine("The values you have selected are:");
-                for (int i = 10; i < arr.Length; i = i + 10)
+                // Ask user if they would like to sort the values before printing to console
+                Console.WriteLine("How would you like to sort the data:\n1) Ascending\n2) Descending\n3) No sort\nResponse: ");
+                string fiftiethValueResponse = Console.ReadLine();
+
+                // The Ascending system is reused here
+                if (fiftiethValueResponse == "Ascending")
                 {
-                    Console.WriteLine(arr[i]);
+                    int z;
+                    for (int p = 0; p <= arr.Length - 2; p++)
+                    {
+                        for (int i = 0; i <= arr.Length - 2; i++)
+                        {
+                            if (arr[i] > arr[i + 1])
+                            {
+                                z = arr[i + 1];
+                                arr[i + 1] = arr[i];
+                                arr[i] = z;
+                            }
+                        }
+                    }
+                    // Print each fiftieth iteration until the max value has been reached
+                    Console.WriteLine();
+                    Console.WriteLine("The values you have selected are:");
+                    for (int i = 50; i < arr.Length; i = i + 50)
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
                 }
-                
+                // The Descending system has been reused here
+                else if (fiftiethValueResponse == "Descending")
+                {
+                    int z;
+                    for (int p = 0; p <= arr.Length - 2; p++)
+                    {
+                        for (int i = 0; i <= arr.Length - 2; i++)
+                        {
+                            if (arr[i] < arr[i + 1])
+                            {
+                                z = arr[i + 1];
+                                arr[i + 1] = arr[i];
+                                arr[i] = z;
+                            }
+                        }
+                    }
+                    // Print each fiftieth iteration until the max value has been reached
+                    Console.WriteLine();
+                    Console.WriteLine("The values you have selected are:");
+                    for (int i = 50; i < arr.Length; i = i + 50)
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
+                }
+                else if (fiftiethValueResponse == "No sort")
+                {
+                    // Print each fiftieth iteration until the max value has been reached
+                    Console.WriteLine();
+                    Console.WriteLine("The values you have selected are:");
+                    for (int i = 50; i < arr.Length; i = i + 50)
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Try again");
+                    DisplayIterativeValues(arr);
+                }
             }
-            else
-            {
-                Console.WriteLine("Try again");
-                DisplayTenthValues(arr);
-            }           
         }
 
-        private static int UserSearch(int[] arr, int searchTarget)
+        private static int UserSearch(int[] arr)
         {
+            // Declare a temporary array, used only in the search 
+            int[] tempArr = new int[arr.Length];
+            Array.Copy(arr, 0, tempArr, 0, arr.Length);
+            // Reuse the system to arrange the values in ascending order
+            int z;
+            for (int p = 0; p <= tempArr.Length - 2; p++)
             {
-                int maxIndex = arr.Length;
+                for (int i = 0; i <= tempArr.Length - 2; i++)
+                {
+                    if (tempArr[i] > tempArr[i + 1])
+                    {
+                        z = tempArr[i + 1];
+                        tempArr[i + 1] = tempArr[i];
+                        tempArr[i] = z;
+                    }
+                }
+            }
+
+            // Ask the user what number they want to search for
+            Console.WriteLine();
+            Console.WriteLine("What number would you like to search for: ");
+            string searchTargetRaw = Console.ReadLine();
+            int searchTarget = Int32.Parse(searchTargetRaw);
+            {
+                int maxIndex = tempArr.Length;
                 // Check if the user's given number is outside the values of the array
-                if (searchTarget <= arr[0])
-                    return arr[0];
-                if (searchTarget >= arr[maxIndex - 1])
+                if (searchTarget <= tempArr[0])
+                    return tempArr[0];
+                if (searchTarget >= tempArr[maxIndex - 1])
                     return arr[maxIndex - 1];
 
                 // Begin doing a binary search to find the closest value 
@@ -147,17 +274,16 @@ namespace Search_and_Sort
                 {
                     midValue = (i + j) / 2;
 
-                    if (arr[midValue] == searchTarget)
-                        return arr[midValue];
+                    if (tempArr[midValue] == searchTarget)
+                        return tempArr[midValue];
 
                     // If the given number is below the midpoint, it will search below this point
-                    if (searchTarget < arr[midValue])
+                    if (searchTarget < tempArr[midValue])
                     {
 
                         // Find the closest value if the given number is greater than the number an index before the midpoint
-                        if (midValue > 0 && searchTarget > arr[midValue - 1])
-                            return GetClosestValue(arr[midValue - 1],
-                                         arr[midValue], searchTarget);
+                        if (midValue > 0 && searchTarget > tempArr[midValue - 1])
+                            return GetClosestValue(tempArr[midValue], tempArr[midValue - 1], searchTarget);                        
 
                         // Repeat this again in the lower sector
                         j = midValue;
@@ -167,15 +293,17 @@ namespace Search_and_Sort
                     else
                     {
                         // Find the closest value if the given number is lower than the number an index after the midpoint
-                        if (midValue < maxIndex - 1 && searchTarget < arr[midValue + 1])
-                            return GetClosestValue(arr[midValue],
-                                 arr[midValue + 1], searchTarget);
+                        if (midValue < maxIndex - 1 && searchTarget < tempArr[midValue + 1])
+                            return GetClosestValue(tempArr[midValue], tempArr[midValue + 1], searchTarget);                        
+                        
+                        // Repeat this again in the upper sector
                         i = midValue + 1;
                     }
                 }
 
                 // Return the only single element remaining after the search     
-                return arr[midValue];
+                return tempArr[midValue];
+                
             }            
         }
 
@@ -186,6 +314,72 @@ namespace Search_and_Sort
                 return valueTwo;
             else
                 return valueOne;
+        }
+
+        public static void DataMenu(int[] roadOne, int[] roadTwo, int[] roadThree, int[] roadOne2048, int[] roadTwo2048, int[] roadThree2048)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Which data set would you like to use:\n1) Road One\n2) Road Two\n3) Road Three\n4) Road One - 2048\n5) Road Two - 2048\n6) Road Three - 2048\nResponse: ");
+            string dataResponse = Console.ReadLine();           
+            if (dataResponse == "Road One")
+            {
+                ProcessMenu(roadOne);
+            }
+            else if (dataResponse == "Road Two")
+            {
+                ProcessMenu(roadTwo);
+            }
+            else if (dataResponse == "Road Three")
+            {
+                ProcessMenu(roadThree);
+            }
+            else if (dataResponse == "Road One - 2048")
+            {
+                ProcessMenu(roadOne2048);
+            }
+            else if (dataResponse == "Road Two - 2048")
+            {
+                ProcessMenu(roadTwo2048);
+            }
+            else if (dataResponse == "Road Three - 2048")
+            {
+                ProcessMenu(roadThree2048);
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+                DataMenu(roadOne, roadTwo, roadThree, roadOne2048, roadTwo2048, roadThree2048);
+            }
+
+        }
+
+        public static void ProcessMenu(int[] arr)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Which process would you like to use:\n1) Ascending Sort\n2) Descending Sort\n3) Display Iterative Values\n4) Value Search\nResponse: ");
+            string processResponse = Console.ReadLine();
+            if (processResponse == "Ascending Sort")
+            {
+                AscendingSort(arr);
+            }
+            else if (processResponse == "Descending Sort")
+            {
+                DescendingSort(arr);
+            }
+            else if (processResponse == "Display Iterative Values")
+            {
+                DisplayIterativeValues(arr);
+            }
+            else if (processResponse == "Value Search")
+            {
+                int closestValue = UserSearch(arr);
+                int valuePosition = Array.IndexOf(arr, closestValue) + 1;
+                Console.WriteLine($"The closest value to your search is {closestValue}, found at position {valuePosition}");
+            }
+            else
+            {
+                ProcessMenu(arr);
+            }
         }
     }
 }
